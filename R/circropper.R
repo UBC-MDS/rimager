e1b610d1f6cd198782386c607f8d4c83b2bfe5f0
haoyusu <haoyusu666@gmail.com>
2020-02-28 10:39:47 -0800

    Added function circropper

---
+++ R/circropper.R
@@ -0,0 +1,13 @@
+#' Crop the image to a circle
+#'
+#' Create a new image array cropped by a circle mask
+#'
+#' @param input_path character
+#' @param outut_path character
+#'
+#' @return an array of image
+#' @export
+#' @examples
+#' circropper("path_to_input_img.png", "output_img.png")
+circropper <- function(x) {
+}
