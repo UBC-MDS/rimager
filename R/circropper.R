4e8190c0198980f6aeb7250c30a5e7f697ee2518
haoyusu <haoyusu666@gmail.com>
2020-03-04 15:40:09 -0800

    added function in circropper.R

--- R/circropper.R
+++ R/circropper.R
@@ -1,13 +1,32 @@
+library(magick)
+
 #' Crop the image to a circle
 #'
-#' Create a new image array cropped by a circle mask
+#' Create a new image cropped by a circle mask and leave a margin
 #'
 #' @param input_path character
 #' @param outut_path character
+#' @param margin double
 #'
-#' @return an array of image
+#' @return an image file saved in the designated folder
 #' @export
 #' @examples
-#' circropper("path_to_input_img.png", "output_img.png")
-circropper <- function(x) {
+#' circropper("path_to_input_img.png", "output_img.png", 20)
+
+circropper <- function(input_path, output_path, margin) {
+  img <- image_read(input_path)
+  height <- image_info(img)$height
+  width <- image_info(img)$width
+
+  png(tf <- tempfile(fileext = ".png"), width, height)
+  par(mar = rep(0,4), yaxs="i", xaxs="i")
+  plot(0, type = "n", ylim = c(0,height), xlim=c(0,width), axes=F, xlab=NA, ylab=NA)
+  plotrix::draw.ellipse(width/2,height/2,width/2-margin,height/2-margin, col="black")
+  dev.off()
+
+
+  mask <- image_read(tf)
+  img_out <- image_composite(mask, img, "plus")
+  image_write(img_out, path = output_path, format = "png")
 }
+
