a704fbd44f8c442260dd70cc5dfc873d935ae243
haoyusu <haoyusu666@gmail.com>
2020-03-06 22:52:37 -0800

    fixed and passed check()

--- tests/testthat/test-circropper.R
+++ tests/testthat/test-circropper.R
@@ -1,3 +1,4 @@
+library(magick)
 input_path <- "https://static0.srcdn.com/wordpress/wp-content/uploads/2019/08/We-Bear-Bears.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5"
 
 test_that("yield error when argument type is wrong", {
@@ -10,14 +11,14 @@ test_that("yield error when margin value is out of scope", {
   expect_error(circropper("tests/milad.jpg", 10000))
 })
 
-img <- image_read(input_path)
-  height <- image_info(img)$height
-width <- image_info(img)$width
-cropped <- circropper(input_path, 0)
-cropped_height <- image_info(cropped)$height
-cropped_width <-image_info(cropped)$width
 
 test_that("circropper works good", {
+  img <- image_read(input_path)
+  height <- image_info(img)$height
+  width <- image_info(img)$width
+  cropped <- circropper(input_path, 0)
+  cropped_height <- image_info(cropped)$height
+  cropped_width <-image_info(cropped)$width
   expect_identical(height, cropped_height)
   expect_identical(width, cropped_width)
 })
