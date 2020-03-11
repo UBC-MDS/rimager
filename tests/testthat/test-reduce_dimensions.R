b2436583d6e1570b1f62a1e6aeff7d613375c270
zhypan <apeggy123@hotmail.com>
2020-03-10 19:12:04 -0700

    resolve conflict

--- tests/testthat/test-reduce_dimensions.R
+++ tests/testthat/test-reduce_dimensions.R
@@ -1,32 +1,11 @@
 test_that("New dimensions are not correct", {
   expect_equal(dim(reduce_dimensions("../mandrill.jpg", "../reduced.jpg", 200, 200)), c(200, 200,3))
-<<<<<<< HEAD
-
-
-=======
   # width : even, height : even number
   
->>>>>>> 3e7441957ecf82759de6cd3c4296f2482824eab7
 })
 
 test_that("New dimensions are not correct", {
   expect_equal(dim(reduce_dimensions("../mandrill.jpg", "../reduced.jpg", 201, 201)), c(201, 201,3))
-<<<<<<< HEAD
-
-
-})
-test_that("New dimensions are not correct", {
-  expect_equal(dim(reduce_dimensions("../mandrill.jpg", "../reduced.jpg", 200, 201)), c(200, 201,3))
-
-
-})
-
-test_that("New dimensions are not correct", {
-  expect_equal(dim(reduce_dimensions("../mandrill.jpg", "../reduced.jpg", 200, 201)), c(200, 201,3))
-
-
-})
-=======
   # width : odd, height : odd number
   
 })
@@ -41,4 +20,3 @@ test_that("New dimensions are not correct", {
   # width : odd, height : odd number
   
 })
->>>>>>> 3e7441957ecf82759de6cd3c4296f2482824eab7
