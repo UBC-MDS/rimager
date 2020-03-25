0554a6c79d65e128b8ac718c08022f7a8455892e
kknebel <keanna.knebel@gmail.com>
2020-03-25 16:56:00 -0700

    Formatted documents using tidyverse styler

--- R/circropper.R
+++ R/circropper.R
@@ -3,7 +3,8 @@
 #' Create a new image cropped by a circle mask and leave a margin
 #'
 #' @param input_path character the image file path
-#' @param margin double the distance between circle boundary and the original image boundary
+#' @param margin double the distance between circle boundary and the original
+#'   image boundary
 #'
 #' @return a cropped image
 #'
@@ -15,30 +16,47 @@
 #'
 #'
 #' @examples
-#' path = system.file("tmp_image", "mandrill.jpg", package = "rimager")
+#' path <- system.file("tmp_image", "mandrill.jpg", package = "rimager")
 #' circropper(path, 20)
 circropper <- function(input_path, margin) {
   # Check argument type
-  if (!is.character(input_path) & !is.double(margin)){
-    stop("TypeError: The 'input_path' arguments must be a character and the 'margin' argument must be a double")}
-  if (!is.character(input_path)){
-    stop("TypeError: The 'input_path' argument must be a character")}
-  if (!is.double(margin)){
-    stop("TypeError: The 'margin' argument must be a double")}
+  if (!is.character(input_path) & !is.double(margin)) {
+    stop("TypeError:
+         * 'input_path' must be a character vector.
+         * 'margin' must be a double vector.")
+  }
+  if (!is.character(input_path)) {
+    stop("TypeError: 'input_path' must be a character vector.")
+  }
+  if (!is.double(margin)) {
+    stop("TypeError: 'margin' must be a double vector.")
+  }
 
   img <- image_read(input_path, strip = TRUE)
   height <- image_info(img)$height
   width <- image_info(img)$width
 
   # Check margin value
-  if (margin > min(height, width)){
-    stop("ValueError: The 'margin' argument should be smaller than ", min(height, width))
+  if (margin > min(height, width)) {
+    stop("ValueError: 'margin' must be smaller than ", min(height, width))
   }
 
   png(tf <- tempfile(fileext = ".png"), width, height)
-  par(mar = rep(0,4), yaxs="i", xaxs="i")
-  plot(0, type = "n", ylim = c(0,height), xlim=c(0,width), axes=F, xlab=NA, ylab=NA)
-  plotrix::draw.ellipse(width/2,height/2,width/2-margin,height/2-margin, col="black")
+  par(mar = rep(0, 4), yaxs = "i", xaxs = "i")
+  plot(0,
+    type = "n",
+    ylim = c(0, height),
+    xlim = c(0, width),
+    axes = FALSE,
+    xlab = NA,
+    ylab = NA
+  )
+  plotrix::draw.ellipse(width / 2,
+    height / 2,
+    width / 2 - margin,
+    height / 2 - margin,
+    col = "black"
+  )
   dev.off()
 
   mask <- image_read(tf)
--- R/redusize.R
+++ R/redusize.R
@@ -1,4 +1,5 @@
-#' A function to reduce the dimension of a give image by removing vertical and horizontal seams
+#' Reduces the dimension of a given image by removing vertical and
+#' horizontal seams
 #'
 #' A new image with the modified width and height will be created
 #'
@@ -14,46 +15,44 @@
 redusize <- function(input_file, output_file, width, height) {
   img <- jpeg::readJPEG(input_file)
 
-  nrows = nrow(img)
-  ncols = ncol(img)
+  nrows <- nrow(img)
+  ncols <- ncol(img)
 
 
   # added exception
-  if (height > ncols) {stop("Value Error: Width should be less than the original width")}
-  if (width > nrows) {stop("Value Error: Height should be less than the original height")}
+  if (height > ncols) {
+    stop("Value Error: Width should be less than the original width")
+  }
+  if (width > nrows) {
+    stop("Value Error: Height should be less than the original height")
+  }
 
 
 
 
-  rem_rows = (nrows - width) %% 2
-  rem_cols = (ncols - height) %% 2
+  rem_rows <- (nrows - width) %% 2
+  rem_cols <- (ncols - height) %% 2
 
   if ((width %% 2) == 0) {
-    remov_rows = ((nrows / 2) - (width / 2) + 1):((nrows / 2) + (width / 2))
-
+    remov_rows <- ((nrows / 2) - (width / 2) + 1):((nrows / 2) + (width / 2))
   } else {
-    remov_rows = floor(((nrows / 2) - (width / 2) + 1)):trunc(((nrows / 2) +
-                                                                 (width / 2)))
+    remov_rows <- floor(((nrows / 2) - (width / 2) + 1)):trunc(((nrows / 2) +
+      (width / 2)))
   }
   if ((height %% 2) == 0) {
-    remov_cols = ((ncols / 2) - (height / 2) + 1):((ncols / 2) + (height / 2))
-
+    remov_cols <- ((ncols / 2) - (height / 2) + 1):((ncols / 2) + (height / 2))
   } else {
-    remov_cols = floor(((ncols / 2)) - (height / 2) + 1):trunc(((ncols / 2)) + (height /
-                                                                                  2))
+    remov_cols <- floor(((ncols / 2)) - (height / 2) + 1):trunc(((ncols / 2)) + (height / 2))
   }
 
-  new_img = array(dim = c(width, height, dim(img)[3]))
+  new_img <- array(dim = c(width, height, dim(img)[3]))
 
 
 
   for (i in 1:dim(img)[3]) {
-    new_img[, , i] = img[, , i][remov_rows, remov_cols]
+    new_img[, , i] <- img[, , i][remov_rows, remov_cols]
   }
 
   jpeg::writeJPEG(new_img, target = output_file)
   return(new_img)
-
-
 }
-
