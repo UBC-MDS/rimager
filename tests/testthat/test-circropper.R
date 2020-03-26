089eec627f1bdf31a61e29e7609dc253e4159f8a
haoyusu <haoyusu666@gmail.com>
2020-03-26 05:58:23 -0700

    added output_path test

--- tests/testthat/test-circropper.R
+++ tests/testthat/test-circropper.R
@@ -1,10 +1,10 @@
-library(magick)
-input_path <- "https://static0.srcdn.com/wordpress/wp-content/uploads/2019/08/We-Bear-Bears.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5"
+input_path <- "../mandrill.jpg"
 
 test_that("yield error when argument type is wrong", {
   expect_error(circropper(1, "one"))
   expect_error(circropper(input_path, "one"))
   expect_error(circropper(1, 1))
+  expect_error(circropper(input_path, 0, 10))
 })
 
 test_that("yield error when margin value is out of scope", {
